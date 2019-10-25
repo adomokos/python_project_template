21d0405970b0c54c35b9a4637009e60b5ed78a75
Attila Domokos <adomokos@gmail.com>
2019-10-25 11:06:59 -0500

    Add the 'context' example

--- spec/tennis_spec.py
+++ spec/tennis_spec.py
@@ -4,9 +4,10 @@ from expects import expect, equal
 from sample.tennis import Game
 
 with description('Tennis') as self:
-    with it('starts with 0 - 0 score'):
-        rafa_nadal = "Rafa Nadal"
-        roger_federer = "Roger Federer"
-        game = Game(rafa_nadal, roger_federer)
+    with context('for an even score'):
+        with it('starts with 0 - 0 score'):
+            rafa_nadal = "Rafa Nadal"
+            roger_federer = "Roger Federer"
+            game = Game(rafa_nadal, roger_federer)
 
-        expect(game.score()).to(equal((0, 0)))
+            expect(game.score()).to(equal((0, 0)))
